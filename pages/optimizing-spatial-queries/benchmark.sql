bb7352db1e60ad529977413d6a298994e186c877
Nik Marchenko <nick.a.march@gmail.com>
2026-01-27 11:22:05 -0800

    call divisions by id

--- pages/optimizing-spatial-queries/benchmark.sql
+++ pages/optimizing-spatial-queries/benchmark.sql
@@ -1,58 +1,58 @@
 \timing on
 
--- Germany
-\echo '=== r51477 (Germany) ==='
+-- Argentina
+\echo '=== Argentina (6aaadea0-9c48-4af0-a47f-bbe020540580) ==='
 
 \echo 'join'
 SELECT COUNT(*) FROM places p
 JOIN divisions d ON ST_Covers(d.geography, p.geography)
-WHERE d.osm_id = 'r51477';
+WHERE d.id = '6aaadea0-9c48-4af0-a47f-bbe020540580';
 
 \echo 'cte'
-WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE osm_id = 'r51477')
+WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE id = '6aaadea0-9c48-4af0-a47f-bbe020540580')
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
 
 \echo 'cte_simplified'
 WITH div AS MATERIALIZED (
     SELECT ST_Buffer(ST_Simplify(geography::geometry, 1000/111342.0), 1000/111342.0)::geography as geog
-    FROM divisions WHERE osm_id = 'r51477'
+    FROM divisions WHERE id = '6aaadea0-9c48-4af0-a47f-bbe020540580'
 )
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
 
--- Chile
-\echo '=== r167454 (Chile) ==='
+-- Deutschland
+\echo '=== Deutschland (d95d3f8a-a2f4-4436-b0e4-a3a86d15008e) ==='
 
 \echo 'join'
 SELECT COUNT(*) FROM places p
 JOIN divisions d ON ST_Covers(d.geography, p.geography)
-WHERE d.osm_id = 'r167454';
+WHERE d.id = 'd95d3f8a-a2f4-4436-b0e4-a3a86d15008e';
 
 \echo 'cte'
-WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE osm_id = 'r167454')
+WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE id = 'd95d3f8a-a2f4-4436-b0e4-a3a86d15008e')
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
 
 \echo 'cte_simplified'
 WITH div AS MATERIALIZED (
     SELECT ST_Buffer(ST_Simplify(geography::geometry, 1000/111342.0), 1000/111342.0)::geography as geog
-    FROM divisions WHERE osm_id = 'r167454'
+    FROM divisions WHERE id = 'd95d3f8a-a2f4-4436-b0e4-a3a86d15008e'
 )
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
 
--- Russia
-\echo '=== r60189 (Russia) ==='
+-- United States
+\echo '=== United States (50a03a2c-3e24-4740-b80d-f933ea60c64f) ==='
 
 \echo 'join'
 SELECT COUNT(*) FROM places p
 JOIN divisions d ON ST_Covers(d.geography, p.geography)
-WHERE d.osm_id = 'r60189';
+WHERE d.id = '50a03a2c-3e24-4740-b80d-f933ea60c64f';
 
 \echo 'cte'
-WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE osm_id = 'r60189')
+WITH div AS MATERIALIZED (SELECT geography as geog FROM divisions WHERE id = '50a03a2c-3e24-4740-b80d-f933ea60c64f')
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
 
 \echo 'cte_simplified'
 WITH div AS MATERIALIZED (
     SELECT ST_Buffer(ST_Simplify(geography::geometry, 1000/111342.0), 1000/111342.0)::geography as geog
-    FROM divisions WHERE osm_id = 'r60189'
+    FROM divisions WHERE id = '50a03a2c-3e24-4740-b80d-f933ea60c64f'
 )
 SELECT COUNT(*) FROM places p, div WHERE ST_Covers(div.geog, p.geography);
