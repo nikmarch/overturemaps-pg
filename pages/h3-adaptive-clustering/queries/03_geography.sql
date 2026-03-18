258a57c4107861a3b17a904b1a23aa5978375605
Nik Marchenko <nick.a.march@gmail.com>
2026-03-18 16:14:05 -0700

    Fix area coverage to use actual cell areas from the MV

--- pages/h3-adaptive-clustering/queries/03_geography.sql
+++ pages/h3-adaptive-clustering/queries/03_geography.sql
@@ -7,8 +7,7 @@ ORDER BY place_count DESC LIMIT 20;
 SELECT res,
   count(*) AS cells,
   sum(place_count) AS places,
-  round(count(*) * h3_cell_area(h3_latlng_to_cell(point(0, 0), res), 'km^2')::numeric) AS approx_area_km2,
-  round(100.0 * count(*) * h3_cell_area(h3_latlng_to_cell(point(0, 0), res), 'km^2')
-    / 510e6, 2) AS pct_earth
+  round(sum(h3_cell_area(cell, 'km^2'))::numeric) AS total_area_km2,
+  round(100.0 * sum(h3_cell_area(cell, 'km^2')) / 510e6, 2) AS pct_earth
 FROM places_h3_t{threshold}
 GROUP BY res ORDER BY res
