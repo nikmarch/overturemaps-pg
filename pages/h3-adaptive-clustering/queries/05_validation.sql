122c2a5c3fcc3e42ed63bd4cdb962b6725313411
Nik Marchenko <nick.a.march@gmail.com>
2026-03-18 20:19:52 -0700

    Optimize validation query with hash join instead of O(n^2) self-join

--- pages/h3-adaptive-clustering/queries/05_validation.sql
+++ pages/h3-adaptive-clustering/queries/05_validation.sql
@@ -1,14 +1,8 @@
--- columns: overlapping_cells, parent_child_pairs
+-- columns: overlapping_cells
 SELECT count(*) AS overlapping_pairs
-FROM places_h3_t{threshold} a
-JOIN places_h3_t{threshold} b
-  ON a.res < b.res
-  AND h3_cell_to_parent(b.cell, a.res) = a.cell;
-
-SELECT a.cell AS parent_cell, a.res AS parent_res, a.place_count AS parent_count,
-  b.cell AS child_cell, b.res AS child_res, b.place_count AS child_count
-FROM places_h3_t{threshold} a
-JOIN places_h3_t{threshold} b
-  ON a.res < b.res
-  AND h3_cell_to_parent(b.cell, a.res) = a.cell
-LIMIT 20
+FROM (
+  SELECT c.cell, h3_cell_to_parent(c.cell, gs) AS parent_cell
+  FROM places_h3_t{threshold} c
+  CROSS JOIN generate_series(1, c.res - 1) AS gs
+) parents
+JOIN places_h3_t{threshold} mv ON mv.cell = parents.parent_cell
