3e5946b47a8ab6884df2fbf3824e77a518eff80a
Nik Marchenko <nick.a.march@gmail.com>
2026-05-18 12:20:10 -0700

    Simplify config to prime/composite types; add odd composites; remove stale results

--- pages/h3-parallel-workers/queries/_config.sql
+++ pages/h3-parallel-workers/queries/_config.sql
@@ -1,9 +1,5 @@
 SELECT
   row_number() OVER (ORDER BY n)::int AS id,
   n AS workers_num,
-  CASE
-    WHEN n IN (2,3,5,7,11,13,17,19,23,29,31) THEN 'prime'
-    WHEN n IN (4,8,16,32)                     THEN 'power_of_2'
-    ELSE                                           'composite'
-  END AS type
-FROM unnest(ARRAY[2,3,4,5,6,7,8,11,12,13,16,17,19,20,23,24,29,30,31,32]) AS t(n);
+  CASE WHEN n IN (2,3,5,7,11,13,17,19,23,29,31) THEN 'prime' ELSE 'composite' END AS type
+FROM unnest(ARRAY[2,3,4,5,6,7,8,13,15,16,21,25,29,35]) AS t(n);
